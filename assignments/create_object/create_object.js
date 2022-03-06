3db000dcc891c08cae3905c8cf3ab835cd01398c
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-03-06 10:03:27 +0530

    playwitharray submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -5,20 +5,17 @@ function CreateObject(arr) {
     for (i=0;i<size;i+=2){
         if (!(arr[i] in d))
         d[arr[i]] = arr[i+1];
-
-        //if(arr[i] not  in d){
-        //}
     } 
-    function sortObjectByKeys(o) {
-    return Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {});
-    }
+    // function sortObjectByKeys(o) {
+    // return Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {});
+    // }
     
-    let st=sortObjectByKeys(d);
+    // let st=sortObjectByKeys(d);
         
         //Object.keys(d).sort().reduce((r, k) => (r[k] = d[k], r), {});
-        return st
+        //return st
         
-    //return d;
+    return d;
     
 
 }
