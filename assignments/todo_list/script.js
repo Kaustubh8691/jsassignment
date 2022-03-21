305278d82a146773911324ad275a1cded3322b1b
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-03-21 17:11:23 +0530

    assignment8

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,39 +1,55 @@
-let count = 0;
-document.querySelector('.addbutton').onclick = function(){
-    let input = document.querySelector('.input').value
-    if(input.length === 0){
-        alert('please enter the value')
-    }else{
-        document.querySelector('.todos').innerHTML += `<p key=${count}>${input}</p>`;
-        count ++
-    }
-    let remover = document.querySelectorAll('.todos>p')
-    for(let i=0; i< remover.length; i++){
-        remover[i].onclick = function(){
-            this.remove();
-        }
-    }
-}
-function removeAll(){
-    document.querySelector('.todos').innerHTML = "";
-}
+let completed =document.getElementById("completed");
+let all=document.getElementById("todos");
+let pending=document.getElementById("pending") ;
 async function apitodo(){
     let res = await fetch('https://jsonplaceholder.typicode.com/todos');
     let data = await res.json();
-    let output = '';
-    let pending = '';
-    data.forEach(function(todo, index) {
-        if(todo.completed===true){
-            output +=`<p key=${index}>${todo.title}</p>`;
-            document.querySelector('.completed').innerHTML = output;
+    
+    for(ele of data){
+        let divElem = document.createElement('div');
+        divElem.innerHTML = `<p><b>${ele.title}</b><p><p>userId:${ele.userId}</p><p>id:${ele.id}</p>`;
+        divElem.setAttribute('style',"width: 45%; margin:5px;border: 1px solid white; padding:5px; display:inline-block;");
+        all.appendChild(divElem);
+        if (ele.completed===true){
+            divElem.setAttribute('class','complete');
+            // divElem.setAttribute('style',"color: rgb(79, 161, 79);")
+            divElem.innerHTML += `<p>status:${ele.completed}`;
+            completed.appendChild(divElem);
+            
         }
-        else if(todo.completed===false){
-            pending +=`<p key=${index}>${todo.title}</p>`;
-            document.querySelector('.pending').innerHTML = pending;
+        else if (ele.completed===false){
+            divElem.setAttribute('class','pend');
+            // divElem.setAttribute('style',"color: rgb(219, 62, 62);")
+            divElem.innerHTML += `<p>status:${ele.completed}`;
+            pending.appendChild(divElem);
+            
         }
-    });
+    }
+
 }
+      
+        
 apitodo();
-// document.querySelector(`p[key="${count}"]`).addEventListener('click',function(){
-//     todolist.removeChild(this)
-// }
+let list=document.getElementById("btn1");
+list.addEventListener("click",showall);
+
+let com=document.getElementById("btn2");
+com.addEventListener("click",showcom) ;
+
+let pen=document.getElementById("btn3");
+pen.addEventListener("click",showpend) ;
+
+function showcom(){
+    pending.setAttribute('style','display:None');
+    completed.setAttribute('style','display:inline-block');
+};
+function showpend(){
+    completed.setAttribute('style','display:None');
+    pending.setAttribute('style','display:inline-block');
+};
+function showall(){
+    all.setAttribute('style','display:inline-block')
+    // completed.setAttribute('style','display:inline-block');
+    // pending.setAttribute('style','display:inline-block');
+};
+      
