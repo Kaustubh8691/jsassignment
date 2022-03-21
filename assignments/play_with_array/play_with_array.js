166ebdc9bd698dcde3d3515ecaa13e7fdbfff148
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-03-21 10:24:37 +0530

    make change

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,15 +7,8 @@ function getEven(arr) {
 
         Write you code below
     */
-   arr.filter((n)=> n%2===0)
-//    let size=arr.length;
-//     let ar=[];
-//     for(i=0;i<size;i++){
-//             if (arr[i]%2==0){
-//             ar.push(arr[i]);
-//   }
-return ar;
-//console.log(ar)
+    ar=arr.filter((n)=>n%2==0);
+    return ar
     
     
 
@@ -29,11 +22,8 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
-   arr.map((x) => x*n)
-//    for (i=0;i<arr.length;i++){
-//        arr[i]=arr[i]*n;
-//    }
-   return arr;
+   ar=arr.map((x)=>x*n);
+   return ar
 }
 
 function removeNthElement(arr, n) {
@@ -44,8 +34,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
-    arr.splice(n, 1);
-    return arr;
+   arr.splice[n,1]
+   return arr
 }
 
 module.exports = {
